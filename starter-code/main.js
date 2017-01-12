646f78d3a0986fbcfd225437bc2efdde36f50402
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-01-12 16:20:02 -0500

    logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,42 +1,17 @@
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
 
-
-/* 
-if (cardOne === cardFour){
-	alert("You found a match!")
-} else{
-	console.log("Sorry, try again.")
+if (cardTwo === cardFour){
+	alert("You found a match!");
 }
-*/
-var deck = 4
-
-function createCards () {
-	for (var i = 0; i < deck; i++){
-		var hereIsCard = document.createElement('div');
-		hereIsCard.className = 'card';
-		document.getElementById('game-board').appendChild(hereIsCard);
-		}
+else if (cardTwo !== cardFour){
+	alert("Sorry, try again.")
 };
-createCards();
-
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
-
- function createBoard(){
- 	for (var i = 0; i < cards.length; i ++){
- 		cards.setAttribute("data-card", cards[i]);
- 	}
- };
-
- function isTwoCards(){
- 	cardsInPlay.push(this.getAttribute("data-card"));
- 	if(cardsInPlay.length === 2){
- 		isMatch(cardsInPlay);
- 		cardsInPlay = [];
- 	}
- };
- for(var i = 0; i < cards.length; i++){
- 		cardElement.addEventListener("click", isTwoCards(););)
- 	};
-
-
-
+if (cardOne === cardTwo){
+	alert("You found a match!");
+}
+else if (cardTwo !== cardFour){
+	alert("Sorry, try again.")
+};
