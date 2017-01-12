d50717adf1a549ba63b5ad8e2c28ff504d23f0c2
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-01-12 16:30:26 -0500

    update with logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -15,3 +15,38 @@ if (cardOne === cardTwo){
 else if (cardTwo !== cardFour){
 	alert("Sorry, try again.")
 };
+
+
+var deck = 4
+
+function createCards () {
+	for (var i = 0; i < deck; i++){
+		var hereIsCard = document.createElement('div');
+		hereIsCard.className = 'card';
+		document.getElementById('game-board').appendChild(hereIsCard);
+		}
+};
+createCards();
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+ function createBoard(){
+ 	for (var i = 0; i < cards.length; i ++){
+ 		cards.setAttribute("data-card", cards[i]);
+ 	}
+ };
+
+ function isTwoCards(){
+ 	cardsInPlay.push(this.getAttribute("data-card"));
+ 	if(cardsInPlay.length === 2){
+ 		isMatch(cardsInPlay);
+ 		cardsInPlay = [];
+ 	}
+ };
+ for(var i = 0; i < cards.length; i++){
+ 	cardElement.addEventListener("click", isTwoCards()(););
+ 	};
+
+
+
