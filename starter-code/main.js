88eec01808f5c6ae41c0d0faf395f77fcda64b43
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-01-16 11:26:34 -0500

    changed size of card omg

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,40 +1,56 @@
-var cards = ["queen", "queen", "king", "king"];
+
+var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
+
+
 var board = document.getElementById('game-board');
 
-function createBoard () {
-	for (var i = 0; i < cards.length; i++){
-		var cardElement = document.createElement('div');
-		cardElement.className = 'card';
-		cardElement.setAttribute('data-card', cards[i]);
-		cardElement.addEventListener('click', isTwoCards);
-		board.appendChild(cardElement);
-    	board.appendChild(cardElement);
-};
+function createBoard() {
+  
+  for (var i=0; i<cards.length; i++) {
+   
+    var cardElement = document.createElement('div');
+   
+    cardElement.className = 'card';
+		
+    cardElement.setAttribute('data-card', cards[i]);
+		
+    cardElement.addEventListener('click', isTwoCards);
 
+   
+    board.appendChild(cardElement);
+    board.appendChild(cardElement);
+  }
 
- function isTwoCards(){
- 	cardsInPlay.push(this.getAttribute("data-card"));
- 	console.log(this.getAttribute('data-card'));
+}
+
+function isTwoCards() {
+  
+  cardsInPlay.push(this.getAttribute('data-card'));
+	
+	console.log(this.getAttribute('data-card'));
 	if (this.getAttribute('data-card') === 'king') {
-		this.innerHTML = "<img src='king.png'>"; 
+		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png' alt = 'king' height = '200px' width = '150px'>"; 
 	} else {
-		this.innerHTML = "<img src='queen.png'>"; 
+		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'alt = 'queen' height = '200px' width = '150px'>"; 
 	}
-	if(cardsInPlay.length === 2){
- 		isMatch(cardsInPlay);
- 		cardsInPlay = [];
- 	}
- };
+  
+  if (cardsInPlay.length === 2) {
+    
+    isMatch(cardsInPlay);
+   
+    cardsInPlay = [];
+  }
+}
 
-function isMatch(cards){
-
-if (cards[0] === cards[1]) {
+function isMatch(cards) {
+  
+  if (cards[0] === cards[1]) {
     alert("You found a match!");
   } else {
     alert("Sorry, try again.");
-	} 
-};
-createBoard();
-};
 
+  }
+}
+
+createBoard();
